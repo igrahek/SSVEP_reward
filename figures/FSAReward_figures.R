c822d9f589441289d7314bcfc4e4ab12aa20aa68
Antonio Schettino <aschetti@protonmail.com>
2018-06-01 21:07:19 +0200

    spectra completed

--- figures/FSAReward_figures.R
+++ figures/FSAReward_figures.R
@@ -2,6 +2,7 @@
 pkgdir <- paste0(getwd(), "/packages") # set package directory
 .libPaths(c(pkgdir, .libPaths())) # add package directory
 
+library(Rmisc) # must be loaded before 'tidyverse' or it will cause compatibility issues
 library(tidyverse)
 library(viridis)
 library(cowplot)
@@ -79,8 +80,147 @@ prow <- plot_grid(topo_10Hz + theme(legend.position = "none"),
 legend <- get_legend(topo_10Hz) # extract legend
 plot_grid(prow, legend, rel_widths = c(3, .3)) # plots & legend
 
+################################################################################################
+########################################### SPECTRA ############################################
+################################################################################################
 
+# adapted from:
+# https://craddm.github.io/2016/11/28/erp-visualization-within-subject-confidence-intervals/
+spectra <- read_csv(paste0(getwd(), "/figures/spectra.csv")) %>% # load data
+  gather(
+    key = frequency, # grouping column
+    value = amplitude, # value column
+    "0":"511.8181818" # columns that need gathering
+  ) %>%
+  mutate(
+    participant = as.factor(participant), # convert participant as factor
+    condition = recode(
+      factor(condition), # convert condition as factor and rename levels
+      "1" = "BslnRedAttended", "2" = "BslnBlueAttended",
+      "3" = "AcqRedAttended", "4" = "AcqBlueAttended",
+      "5" = "ExtRedAttended", "6" = "ExtBlueAttended"
+    ),
+    frequency = as.numeric(frequency) # convert frequency as numeric
+  ) %>%
+  filter(frequency <= 16) # filter out frequencies not in graph
 
+# summarized data from each time point, including within-subject 95% CIs
+spectra.pointsummary <- spectra %>%
+  split(.$frequency) %>%
+  map(~ summarySEwithin(
+    data = .,
+    measurevar = "amplitude",
+    withinvars = "condition",
+    idvar = "participant"
+  ))
 
+# extract within-subject 95% CIs
+spectra..withinssjCI <- map_df(spectra.pointsummary, magrittr::extract) %>%
+  mutate(frequency = rep(unique(spectra$frequency), each = length(unique(spectra$condition))))
 
+# plot (frequency range: 0-16 Hz)
+ggplot(spectra, aes(frequency, amplitude)) + # basic plot
+  stat_summary(aes(colour = condition), # lines: means
+    fun.y = mean,
+    geom = "line",
+    size = 1.3
+  ) +
+  geom_ribbon( # ribbons: 95% CI
+    data = spectra..withinssjCI,
+    aes(ymin = amplitude - ci, ymax = amplitude + ci, fill = condition),
+    linetype = "solid",
+    alpha = .2
+  ) +
+  scale_color_viridis(discrete = TRUE) +
+  scale_fill_viridis(discrete = TRUE) +
+  geom_vline(xintercept = seq(0, 16, 2), linetype = "dotted", colour = "#999999", size = .8, alpha = .5) +
+  geom_hline(yintercept = seq(0, 1.6, .2), linetype = "dotted", colour = "#999999", size = .8, alpha = .5) +
+  scale_y_continuous(breaks = seq(0, 1.6, .2)) +
+  scale_x_continuous(breaks = seq(0, 16, 2)) +
+  labs(
+    title = "Amplitude (0 - 16 Hz)",
+    x = "frequency (Hz)",
+    y = expression(paste("amplitude (", mu, "V)"))
+  ) +
+  guides(fill = "none", color = guide_legend(title = NULL)) +
+  theme_classic(base_size = 18) +
+  theme(
+    legend.position = c(.2, .8),
+    plot.title = element_text(size = 24, hjust = .5)
+  )
 
+# plot (frequency range: 9-11 Hz)
+filter(spectra, frequency >= 9 & frequency <= 11) %>%
+  ggplot(., aes(frequency, amplitude)) +
+  stat_summary(aes(colour = condition),
+    fun.y = mean,
+    geom = "line",
+    size = 1.3
+  ) +
+  geom_ribbon(
+    data = filter(spectra..withinssjCI, frequency >= 9 & frequency <= 11),
+    aes(ymin = amplitude - ci, ymax = amplitude + ci, fill = condition),
+    linetype = "solid",
+    alpha = .2
+  ) +
+  scale_color_viridis(discrete = TRUE) +
+  scale_fill_viridis(discrete = TRUE) +
+  geom_vline(xintercept = seq(9, 11, 1), linetype = "dotted", colour = "#999999", size = .8, alpha = .5) +
+  geom_hline(yintercept = seq(0, 1.6, .2), linetype = "dotted", colour = "#999999", size = .8, alpha = .5) +
+  scale_y_continuous(breaks = seq(0, 1.6, .2)) +
+  scale_x_continuous(breaks = seq(9, 11, 1)) +
+  coord_cartesian(
+    ylim = c(.6, 1.6), # zoom in
+    xlim = c(9.5, 10.5)
+  ) +
+  labs(
+    title = "Amplitude (10 Hz)",
+    x = "frequency (Hz)",
+    y = expression(paste("amplitude (", mu, "V)"))
+  ) +
+  guides(fill = "none", color = guide_legend(title = NULL)) +
+  theme_classic(base_size = 18) +
+  theme(
+    legend.position = c(.2, .8),
+    plot.title = element_text(size = 24, hjust = .5)
+  )
+
+# plot (frequency range: 11-13 Hz)
+filter(spectra, frequency >= 11 & frequency <= 13) %>%
+  ggplot(., aes(frequency, amplitude)) +
+  stat_summary(aes(colour = condition),
+    fun.y = mean,
+    geom = "line",
+    size = 1.3
+  ) +
+  geom_ribbon(
+    data = filter(spectra..withinssjCI, frequency >= 11 & frequency <= 13),
+    aes(ymin = amplitude - ci, ymax = amplitude + ci, fill = condition),
+    linetype = "solid",
+    alpha = .2
+  ) +
+  scale_color_viridis(discrete = TRUE) +
+  scale_fill_viridis(discrete = TRUE) +
+  geom_vline(xintercept = seq(11, 13, 1), linetype = "dotted", colour = "#999999", size = .8, alpha = .5) +
+  geom_hline(yintercept = seq(.5, 1, .1), linetype = "dotted", colour = "#999999", size = .8, alpha = .5) +
+  scale_y_continuous(breaks = seq(.5, 1, .1)) +
+  scale_x_continuous(breaks = seq(11, 13, 1)) +
+  coord_cartesian(
+    ylim = c(.5, 1), # zoom in
+    xlim = c(11.5, 12.5)
+  ) +
+  labs(
+    title = "Amplitude (12 Hz)",
+    x = "frequency (Hz)",
+    y = expression(paste("amplitude (", mu, "V)"))
+  ) +
+  guides(fill = "none", color = guide_legend(title = NULL)) +
+  theme_classic(base_size = 18) +
+  theme(
+    legend.position = c(.2, .8),
+    plot.title = element_text(size = 24, hjust = .5)
+  )
+
+################################################################################################
+################################################################################################
+################################################################################################
