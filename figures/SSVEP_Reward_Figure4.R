99df8897afd00cf2f81b12b24ab5202e7bba68ee
Antonio Schettino <antonio.schettino@posteo.net>
2020-02-07 18:43:27 +0100

    Figure 4 redone

--- figures/SSVEP_Reward_Figure4.R
+++ figures/SSVEP_Reward_Figure4.R
@@ -1,32 +1,40 @@
 
 ######################################### SETUP #########################################
 
-# .libPaths("E:/R_workspace/packages") # add package directory (only on my Windows machine)
-
-setwd(paste0(getwd(), "/figures/")) # when using R projects
-# setwd("E:/Experiments/Grahek_Ivan/FSAReward/repo/figures")
-
 ### install packages
-# install.packages(c("tidyverse", "Rmisc",
-#                    "parallel", "rstan", "brms",
-#                    "viridis", "cowplot"),
-#                  dependencies = TRUE)
-# devtools::install_github('craddm/eegUtils', dependencies = TRUE)
-# devtools::install_github("mikabr/ggpirate", dependencies = TRUE)
+# install.packages("here")
+# install.packages("Rmisc")
+# install.packages("tidyverse")
+# install.packages("viridis")
+# install.packages("cowplot")
+# install.packages("devtools")
+# devtools::install_github("r-lib/conflicted")
+# devtools::install_github('craddm/eegUtils')
+# devtools::install_github("mikabr/ggpirate")
 
 ### load packages
-library(Rmisc) # must be loaded before 'tidyverse' or it will cause compatibility issues
+library(conflicted)
+library(here)
+library(Rmisc)
 library(tidyverse)
 library(viridis)
 library(cowplot)
 library(eegUtils)
 library(ggpirate)
 
-########################################### FIGURE 2 ###########################################
+# avoid function conflicts
+conflict_prefer("here", "here")
+conflict_prefer("filter", "dplyr")
+conflict_prefer("rename", "dplyr")
+conflict_prefer("mutate", "dplyr")
+conflict_prefer("select", "dplyr")
+conflict_prefer("summarize", "dplyr")
+
+########################################### FIGURE 4 ###########################################
 ######################################### TOPOGRAPHIES #########################################
 
 # load electrode locations
-electrodeLocs <- read_delim(paste0(getwd(), "/BioSemi64.locs"),
+electrodeLocs <- read_delim(here("figures/BioSemi64.locs"),
   "\t",
   col_names = c("chanNo", "theta", "radius", "electrode"),
   escape_double = FALSE, trim_ws = TRUE
@@ -38,7 +46,7 @@ electrodeLocs <- read_delim(paste0(getwd(), "/BioSemi64.locs"),
     y = radius * cos(radianTheta)
   )
 
-topos <- read_csv(paste0(getwd(), "/grandAverage_topos.csv")) %>%
+topos <- read_csv(here("figures/grandAverage_topos.csv")) %>%
   bind_cols(., electrodeLocs) %>% # bind electrode locations and amplitudes
   select(c("10 Hz", "12 Hz", "electrode", "x", "y"))
 
@@ -56,7 +64,7 @@ topo_10Hz <-
     contour = TRUE,
     chan_marker = "point",
     quantity = "amplitude",
-    highlights = c("IZ", "OZ", "POZ", "O2")
+    highlights = c("Iz", "Oz", "POz", "O2")
   ) +
   ggtitle("10 Hz") +
   guides(fill = guide_colorbar(
@@ -85,7 +93,7 @@ topo_12Hz <-
     contour = TRUE,
     chan_marker = "point",
     quantity = "amplitude",
-    highlights = c("IZ", "OZ", "POZ", "O2")
+    highlights = c("Iz", "Oz", "POz", "O2")
   ) +
   ggtitle("12 Hz") +
   guides(fill = guide_colorbar(
@@ -104,8 +112,8 @@ topo_12Hz <-
 
 # adapted from:
 # https://craddm.github.io/2016/11/28/erp-visualization-within-subject-confidence-intervals/
-spectra <- read_csv(paste0(getwd(), "/grandAverage_spectra.csv")) %>% # load data
-  dplyr::select(-c(`16.0455`:`30`)) %>% # keep frequencies until 16 Hz for plotting
+spectra <- read_csv(here("figures/grandAverage_spectra.csv")) %>% # load data
+  select(-c(`16.0455`:`30`)) %>% # keep frequencies until 16 Hz for plotting
   gather(
     key = frequency,
     value = amplitude,
@@ -124,7 +132,9 @@ spectra <- read_csv(paste0(getwd(), "/grandAverage_spectra.csv")) %>% # load dat
     ),
     condition = as.factor(paste(Phase, attended, sep = " "))
   ) %>%
-  dplyr::select(participant, frequency, condition, Phase, attended, amplitude)
+  select(participant, frequency, condition, Phase, attended, amplitude) %>%
+  filter(participant != "14") %>% # participant eliminated from analyses due to noisy signal
+  droplevels()
 
 # extract within-subject 95% CIs
 spectra_withinssjCI <- spectra %>%
@@ -137,7 +147,7 @@ spectra_withinssjCI <- spectra %>%
     idvar = "participant"
   )) %>%
   mutate(frequency = as.numeric(rep(unique(spectra$frequency), each = length(unique(spectra$condition))))) %>%
-  dplyr::select(frequency, condition, Phase, attended, N, amplitude, ci)
+  select(frequency, condition, Phase, attended, N, amplitude, ci)
 
 # plot (frequency range: 0-16 Hz)
 spectra_all <-
@@ -166,51 +176,108 @@ spectra_all <-
   coord_cartesian(xlim = c(0, 16), ylim = c(0, 1.5)) +
   labs(
     x = "frequency (Hz)",
-    y = expression(paste("amplitude (", mu, "V)"))
-    ) +
+    y = "amplitude (a.u.)"
+  ) +
   theme_classic(base_size = 18) +
   theme(
     legend.position = c(.2, .7),
     plot.title = element_text(size = 22, hjust = .5)
   )
 
+########################################### RDI PLOT ###########################################
+
+ssVEP_RDI <-
+  spectra %>%
+  filter(frequency %in% c(10, 12)) %>%
+  mutate(frequency = recode(factor(frequency),
+    "10" = "10 Hz", "12" = "12 Hz"
+  )) %>%
+  ggplot(aes(
+    x = Phase,
+    y = amplitude,
+    color = attended,
+    fill = attended
+  )) +
+  geom_pirate(
+    bars = FALSE,
+    cis = TRUE,
+    lines = TRUE, lines_params = list(color = "black"),
+    points = TRUE, points_params = list(shape = 21, size = 5, alpha = .1),
+    violins = TRUE, violins_params = list(size = 1),
+    show.legend = TRUE
+  ) +
+  scale_color_manual(values = rep(c("red", "blue"), 3)) +
+  scale_fill_manual(values = rep(c("red", "blue"), 3)) +
+  scale_y_continuous(
+    limits = c(0, 5),
+    breaks = seq(0, 5, 1)
+  ) +
+  ylab("amplitude (a.u.)") +
+  facet_wrap(~frequency) +
+  theme_minimal(base_size = 16) +
+  theme(
+    strip.text = element_text(
+      hjust = .5,
+      size = 20
+    ),
+    plot.title = element_text(size = 26, hjust = .5),
+    legend.box.background = element_rect(color = "transparent"),
+    legend.position = "right"
+  )
+
 ######################################## ARRANGE PLOTS #########################################
 
-# arrange topographies in a single row
-topo_row_temp <- plot_grid(topo_10Hz + theme(legend.position = "none"),
-  topo_12Hz + theme(legend.position = "none"),
-  align = "vh",
-  hjust = -1,
-  nrow = 1
-)
+# panel A: spectra
+cowplot_spectra_all <-
+  plot_grid(spectra_all,
+    labels = "A"
+  )
 
-legend <- get_legend(topo_10Hz) # extract legend
+# panel B: topographies
+cowplot_topos <-
+  plot_grid(topo_10Hz + theme(legend.position = "none"),
+    topo_12Hz + theme(legend.position = "none"),
+    align = "vh",
+    hjust = -1,
+    nrow = 2,
+    labels = "C"
+  )
 
-topo_row <- plot_grid(topo_row_temp,
-  legend,
-  rel_widths = c(3, .3),
-  labels = "A"
-)
+# extract legend
+cowplot_legend <-
+  get_legend(topo_10Hz + theme(legend.box.margin = margin(0, 30, 0, 0)))
 
-# add label to spectra_all
-spectra_all <- plot_grid(spectra_all,
-  labels = "B"
-)
+# unite plot and legend
+cowplot_topos_legend <-
+  plot_grid(cowplot_topos,
+    cowplot_legend,
+    rel_widths = c(1, .1)
+  )
 
-# all plots
-figure_4 <- plot_grid(topo_row,
-  spectra_all,
-  ncol = 1,
-  nrow = 2
-)
+# panel C: RDI plot
+cowplot_ssVEP_RDI <-
+  plot_grid(ssVEP_RDI,
+    labels = "B"
+  )
+
+# all plots in one figure
+figure_4 <-
+  plot_grid(cowplot_spectra_all,
+    plot_grid(cowplot_ssVEP_RDI,
+      cowplot_topos_legend,
+      ncol = 2,
+      rel_widths = c(1.3, .7)
+    ),
+    ncol = 1
+  )
 
 # save as tiff
-save_plot(paste0(getwd(), "/Figure4.tiff"),
-  figure_4,
-  base_height = 10,
-  base_aspect_ratio = 1.1
+ggsave(
+  plot = figure_4,
+  filename = here("figures/Figure4.tiff"),
+  units = "in",
+  width = 12, height = 10,
+  dpi = 600, compression = "lzw"
 )
 
 ###################################################################################################
-
-
